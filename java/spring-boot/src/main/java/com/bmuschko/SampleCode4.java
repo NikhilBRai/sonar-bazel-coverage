19d9ea96e506a910e5899ee2b292e9b68cc03819
nikhilbrai@gmail.com <nikhil.rai@nikhilr-ltm9ej9.internal.salesforce.com>
2024-09-13 23:14:52 +0530

    test coverage link

--- java/spring-boot/src/main/java/com/bmuschko/SampleCode4.java
+++ java/spring-boot/src/main/java/com/bmuschko/SampleCode4.java
@@ -4,7 +4,7 @@ public class SampleCode4 {
     public Boolean sampleMethod() {
         System.out.println("Hey1");
         System.out.println("Hey2");
-        System.out.println("Hey4");
+        System.out.println("Hey5");
         return true;
     }
 }
