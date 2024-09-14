1465e64cad47a6597ef091401df32746cb32e6c7
nikhilbrai@gmail.com <nikhil.rai@nikhilr-ltm9ej9.internal.salesforce.com>
2024-09-14 13:43:55 +0530

    Testing

--- java/spring-boot/src/main/java/com/bmuschko/SampleCode6.java
+++ java/spring-boot/src/main/java/com/bmuschko/SampleCode6.java
@@ -4,7 +4,7 @@ public class SampleCode6 {
     public Boolean sampleMethod() {
         System.out.println("Hey1");
         System.out.println("Hey2");
-        System.out.println("Hey4");
+        System.out.println("Hey4"); //
         return true;
     }
 }
