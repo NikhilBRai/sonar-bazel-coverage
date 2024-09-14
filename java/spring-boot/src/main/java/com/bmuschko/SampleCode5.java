3289d9557228fb3a788136453d0c0f08975fcf11
nikhilbrai@gmail.com <nikhil.rai@nikhilr-ltm9ej9.internal.salesforce.com>
2024-09-14 12:32:34 +0530

    Testing code changes

--- java/spring-boot/src/main/java/com/bmuschko/SampleCode5.java
+++ java/spring-boot/src/main/java/com/bmuschko/SampleCode5.java
@@ -1,9 +1,10 @@
 package com.bmuschko;
 
-public class SampleCode4 {
+public class SampleCode5 {
     public Boolean sampleMethod() {
         System.out.println("Hey1");
-        System.out.println("Hey3");
+        System.out.println("Hey2");
+        System.out.println("Hey4");
         return true;
     }
 }
